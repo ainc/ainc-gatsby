ace771b9d0748101c696005d87b62d8dd75ff71c
Kyle Raney <raney24fcb@gmail.com>
2022-04-07 16:32:33 -0400

    fix 5 across page

--- src/pages/5across/components/PastWinners/PastWinners.js
+++ src/pages/5across/components/PastWinners/PastWinners.js
@@ -1,19 +1,23 @@
 import React from "react";
+import { graphql } from "gatsby";
 import * as styles from "./PastWinners.module.css";
 import BrandButton from "../../../../components/UI/BrandButton/BrandButton";
 
 const PastWinners = ({ year, winner, founder, link }) => {
+  
   return (
     <div className={`text-center ${styles.border}`}>
       <h4 className={`text-center align-items-end ${styles.subHeading}`}>{year}</h4>
       <p className={`${styles.content} ${styles.contentWinner}`}>{winner}</p>
       <p className={`${styles.content} ${styles.contentFounders}`}>Founders:</p>
       {/* maps through the array of founders names. */}
-      {founder.map((founders) => {
-        return founders?.map((foundingMember) => {
-          return <p className={styles.content}>{foundingMember}</p>;
-        });
-      })}
+      {
+        founder.map((founders) => {
+          return founders?.map((foundingMember) => {
+            return <p className={styles.content}>{foundingMember}</p>;
+          });
+        })
+      }
       <div className="d-flex justify-content-center">
         <div className={styles.absolute}>
           <BrandButton
